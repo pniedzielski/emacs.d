ba3f4290e31835907bf3aef1a4f711925da74205
Patrick M. Niedzielski <patrick@pniedzielski.net>
2021-08-25 06:32:10 +0000

    Add framework for configuration
    License: GPLv3+

---
+++ init.el
@@ -0,0 +1,7 @@
+(require 'org)
+(letrec ((org-file (expand-file-name "configuration.org" user-emacs-directory))
+         (el-file  (concat (file-name-sans-extension org-file) ".el"))
+         (elc-file (concat (file-name-sans-extension el-file) ".elc")))
+  (if (file-readable-p elc-file)
+      (load-file elc-file)
+    (org-babel-load-file org-file)))
