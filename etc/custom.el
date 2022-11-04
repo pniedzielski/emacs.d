cf45c09dc86460d05f705ef91a54234f15f83750
Patrick M. Niedzielski <patrick@pniedzielski.net>
2022-11-04 08:41:14 +0000

    Move configuration files to more sensible locations
    Using the `no-littering` package, we can simplify our configuration
    while tidying up our `.emacs.d`.
    
    License: GPLv3+

---
+++ etc/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("156203aa09d9e51c16c1ebded562a07b84b761361c1bdeb5a6102abe1ba16158" "fa5b0e1b09262620e31dda600e03d11ad402ee42fb5178c27325e1aeac78ab5b" "86aa44147372e9868eb707246be5c46be0aa861706ba48e0b8463043f6ba1e21" "5e2cdea6453f8963037723ab91c779b203fb201bf5c377094440f0c465d688ec" default))
+ '(ledger-default-date-format "%Y-%m-%d")
+ '(package-selected-packages
+   '(no-littering kind-icon corfu-doc corfu ol-notmuch unicode-math-input emacsql-sqlite-builtin ibuffer-project yaml-mode embark-consult embark password-store elfeed-protocol elfeed-goodies elfeed consult ledger-mode org-pomodoro marginalia all-the-icons-completion all-the-icons-ibuffer all-the-icons-dired all-the-icons vertico magit-annex org-roam-ui notmuch cmake-mode which-key use-package typo tree-sitter-langs tangotango-theme pdf-tools org-roam org-agenda-property multiple-cursors magit-imerge magit-filenotify haskell-mode free-keys form-feed forge expand-region exec-path-from-shell eglot ebib dimmer diminish color-theme-modern auto-dictionary auctex amx alert))
+ '(tab-bar-format
+   '(tab-bar-format-menu-bar tab-bar-format-history tab-bar-format-tabs tab-bar-separator)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:extend nil :foreground "dodger blue" :weight bold))))
+ '(tab-bar-tab-inactive ((t (:inherit mode-line-inactive)))))
